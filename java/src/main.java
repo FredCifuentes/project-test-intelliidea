bd0d1133d7c34a42b6d02bd5b2dbd8462a5266a7
Fred Cifuentes <fredy97daniel@gmail.com>
2025-09-30 21:57:49 -0600

    segundo commit

--- java/src/main.java
+++ java/src/main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // Mensaje en consola
         System.out.println("¡Hola, mundo!");
+        System.out.println("¡Probando Git!");
     }
 }
